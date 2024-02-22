670774afdae0f7bb8debf77a701232a8cd21f6e9
Dmitriy <klimov.dima@mail.ru>
2024-02-22 15:18:12 +0300

    Vue cli

---
+++ Практика/Лекции/Vue cli/first-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome"/>
+    <CalcComponent></CalcComponent>
+    <h2>Lorem ipsum dolor sit.</h2>
+    <CalcComponent></CalcComponent>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import CalcComponent from './components/CalcComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    CalcComponent
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
