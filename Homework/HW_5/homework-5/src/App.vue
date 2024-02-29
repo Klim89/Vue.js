fe00ba85b72460ce2bd60a2e294bc2802e27685c
Dmitriy <klimov.dima@mail.ru>
2024-02-29 17:18:25 +0300

    HW_5

--- Homework/HW_5/homework-5/src/App.vue
+++ Homework/HW_5/homework-5/src/App.vue
@@ -1,19 +1,42 @@
 <template>
   <div id="app">
+    <HeaderComp></HeaderComp>
+    <FooterComp></FooterComp>
   </div>
 </template>
 
 <script>
-
+import HeaderComp from './components/HeaderComp.vue';
+import FooterComp from './components/FooterComp.vue';
 
 export default {
   name: 'App',
   components: {
-
+    HeaderComp,
+    FooterComp
   }
 }
 </script>
 
 <style lang="scss">
+$widthContent: 1200px;
 
+html {
+  scroll-behavior: smooth;
+  font-size: 16px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  text-decoration: none;
+}
+
+.center {
+  padding-left: calc(50% - $widthContent / 2);
+  padding-right: calc(50% - $widthContent / 2);
+}
 </style>
