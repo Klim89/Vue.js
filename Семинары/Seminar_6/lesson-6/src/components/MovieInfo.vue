16df6d868e76b78517ca89e5ac7bfc5753337cf3
Dmitriy <klimov.dima@mail.ru>
2024-03-05 01:22:14 +0300

    Seminar_6

---
+++ Семинары/Seminar_6/lesson-6/src/components/MovieInfo.vue
@@ -0,0 +1,47 @@
+<template>
+  <li @click="toggleDetails(movie)">
+    <h3>Выбранный фильм</h3>
+    <p>Название: {{ movie.title }}</p>
+    <p>Режиссер: {{ movie.director }}</p>
+    <p>Год выпуска: {{ movie.year }}</p>
+<!--    <div v-if="movie.showDetails">-->
+<!--      <p>{{ movie.details }}</p>-->
+<!--      &lt;!&ndash; Дополнительные детали о книге &ndash;&gt;-->
+<!--      <p v-if="availability">Нет дополнителных деталей о книге</p>-->
+<!--    </div>-->
+  </li>
+</template>
+
+<script>
+
+import Vue from "vue";
+
+export default {
+  name: "MovieInfo",
+  props: {
+    movie: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      availability: false,
+    }
+  },
+  methods: {
+    toggleDetails(movie) {
+      Vue.set(movie, 'showDetails', !movie.showDetails);
+      if (!movie.details) {
+        this.availability = !this.availability
+      }
+      this.$emit('info-updated', movie.details);
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ Семинары/Seminar_6/lesson-6/src/components/MovieList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>Список фильмов</h2>
+    <ul>
+      <MovieInfo :movie="movie" v-for="movie in movies" :key="movie.id" @info-updated="handleInfoUpdate"/>
+    <div>{{ newDetails }}</div>
+    </ul>
+  </div>
+</template>
+
+<script>
+import MovieInfo from './MovieInfo.vue'
+
+export default {
+  name: 'MovieList',
+  components: {
+    MovieInfo
+  },
+  props: {},
+  data() {
+    return {
+      movies: [
+        {id: 1, title: 'Фильм 1', director: 'Режиссер 1', year: 2020, details: 'Какие то детали'},
+        {id: 2, title: 'Фильм 2', director: 'Режиссер 2', year: 2019, details: ''},
+        {id: 3, title: 'Фильм 3', director: 'Режиссер 3', year: 2021, details: 'И тут детали'}
+      ],
+      newDetails: ''
+    };
+  },
+  methods: {
+    handleInfoUpdate(details) {
+      this.newDetails = details;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
