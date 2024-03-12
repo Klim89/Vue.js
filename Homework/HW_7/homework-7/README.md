a9a5544edd6cdb636aa883333f2f174aa176f12c
Dmitriy <klimov.dima@mail.ru>
2024-03-12 19:05:01 +0300

    HW_7

---
+++ Homework/HW_7/homework-7/README.md
@@ -0,0 +1,29 @@
+# homework-7
+
+Установите VueX и добавьте к проекту.
+Приступаем к вёрстке новой страницы project details (https://www.figma.com/file/okdYD45Tj2JpKsNASccUmf/Interior-Design-Webflow-Website-Template-(Community)-(Copy)-(Copy)?type=design&node-id=1418-15&mode=design&t=ebkazpiG5BOth8x7-0):
+
+Информацию про описание проекта вынести в state.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ Homework/HW_7/homework-7/src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="app">
+    <div class="homepage">
+      <HeaderComp></HeaderComp>
+      <HomeBanner></HomeBanner>
+      <HomeProject></HomeProject>
+      <HomeInfo></HomeInfo>
+      <HomeArticle></HomeArticle>
+      <FooterComp></FooterComp>
+    </div>
+    <div class="blog-page">
+      <HeaderComp></HeaderComp>
+      <BlogBanner></BlogBanner>
+      <BlogPost></BlogPost>
+      <BlogArticle></BlogArticle>
+      <BlogPagination></BlogPagination>
+      <FooterComp></FooterComp>
+    </div>
+    <div class="blog-details">
+      <HeaderComp></HeaderComp>
+      <BlogDetailsBanner></BlogDetailsBanner>
+      <BlogDetailsArticle></BlogDetailsArticle>
+      <FooterComp></FooterComp>
+    </div>
+    <div class="project-page">
+      <HeaderComp></HeaderComp>
+      <ProjectBanner></ProjectBanner>
+      <ProjectMainContent></ProjectMainContent>
+      <BlogPagination></BlogPagination>
+      <FooterComp></FooterComp>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderComp from './components/HeaderComp.vue';
+import FooterComp from './components/FooterComp.vue';
+import HomeProject from './components//HomePage/HomeProject.vue';
+import HomeBanner from './components/HomePage/HomeBanner.vue';
+import HomeInfo from './components/HomePage/HomeInfo.vue';
+import HomeArticle from './components/HomePage/HomeArticle.vue';
+import BlogBanner from './components/BlogPage/BlogBanner.vue';
+import BlogPost from './components/BlogPage/BlogPost.vue';
+import BlogArticle from './components/BlogPage/BlogArticle.vue';
+import BlogPagination from './components/BlogPage/BlogPagination.vue';
+import BlogDetailsBanner from './components/BlogDetails/BlogDetailsBanner.vue';
+import BlogDetailsArticle from './components/BlogDetails/BlogDetailsArticle.vue';
+import ProjectBanner from './components/ProjectPage/ProjectBanner.vue';
+import ProjectMainContent from './components/ProjectPage/ProjectMainContent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComp,
+    FooterComp,
+    HomeProject,
+    HomeBanner,
+    HomeInfo,
+    HomeArticle,
+    BlogBanner,
+    BlogPost,
+    BlogArticle,
+    BlogPagination,
+    BlogDetailsBanner,
+    BlogDetailsArticle,
+    ProjectBanner,
+    ProjectMainContent
+  }
+}
+</script>
+
+<style lang="scss">
+$widthContent: 1200px;
+
+html {
+  scroll-behavior: smooth;
+  font-size: 16px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  text-decoration: none;
+}
+
+.center {
+  padding-left: calc(50% - $widthContent / 2);
+  padding-right: calc(50% - $widthContent / 2);
+}
+</style>
